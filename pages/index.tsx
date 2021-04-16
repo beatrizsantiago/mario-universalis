1a476862121fb7e7ab8e5bebff534cdce1fc5785
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2021-04-15 22:00:35 -0300

    💄 Add GlobalStyle in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 import client from './api';
 import { DEVICES } from './api/queries';
 import DevicesList from '../src/components/Home/DevicesList';
 import Header from '../src/components/Home/Header';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
 
 export async function getServerSideProps() {
   const { data } = await client.query({
@@ -18,6 +25,7 @@ export async function getServerSideProps() {
 
 const Home: React.FC = ({ devices }) => (
   <>
+    <GlobalStyle />
     <Header />
     <DevicesList devices={devices} />
   </>
