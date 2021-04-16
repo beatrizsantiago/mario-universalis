abded3b54aef285c0aa4780986e39c85e5d97198
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2021-04-15 22:02:19 -0300

    💄 Add gradient background to Header component

--- src/components/Home/Header/styles.tsx
+++ src/components/Home/Header/styles.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Content = styled.div`
   width: 100%;
   height: 240px;
+  background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,212,255,1) 80%);
 `;
 
 export default Content;
