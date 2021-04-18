98024781cec84170d015f3e19eb6d88942ccd195
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2021-04-18 15:45:04 -0300

    🎨 🔥 Remove GlobalStyles of index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
 import client from './api';
 import { DEVICES } from './api/queries';
 import DevicesList from '../src/components/Home/DevicesList';
 import Header from '../src/components/Home/Header';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-  }
-`;
 
 export async function getServerSideProps() {
   const { data } = await client.query({
@@ -25,7 +18,6 @@ export async function getServerSideProps() {
 
 const Home: React.FC = ({ devices }) => (
   <>
-    <GlobalStyle />
     <Header />
     <DevicesList devices={devices} />
   </>
