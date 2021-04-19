29656b10afb6fcd93465006eece9aedd6d9d662a
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2021-04-19 08:54:27 -0300

    🚧 ✨ Add Pagination component

---
+++ src/components/Home/Pagination/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Title } from './styles';
+
+const Pagination = () => (
+  <Box>
+    <Title>Devices List</Title>
+  </Box>
+);
+
+export default Pagination;
---
+++ src/components/Home/Pagination/styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Box = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 40px;
+`;
+
+export const Title = styled.h1`
+  font-size: 25px;
+  font-weight: 700;
+  color: #ffffff;
+  line-height: 1;
+`;
