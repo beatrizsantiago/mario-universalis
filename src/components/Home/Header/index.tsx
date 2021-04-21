16a453b50369ec54350b03eb179a4a2902158837
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2021-04-21 15:23:15 -0300

    ✨ Add title to Pagination in Header component

--- src/components/Home/Header/index.tsx
+++ src/components/Home/Header/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Pagination from '../Pagination';
+import Pagination from '../../Pagination';
 import { Content, GradientBackground } from './styles';
 
 const Header = () => (
   <Content>
     <GradientBackground>
-      <Pagination />
+      <Pagination title="Devices List" />
     </GradientBackground>
   </Content>
 );
