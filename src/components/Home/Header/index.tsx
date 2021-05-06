2a6ba3722ff41c8aeee7bbd9d9286fc8c17c2b15
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2021-05-05 21:18:08 -0300

    ✨ Add NavBar to Header component

--- src/components/Home/Header/index.tsx
+++ src/components/Home/Header/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Pagination from '../../Pagination';
+import NavBar from '../../NavBar';
 import { Content, GradientBackground } from './styles';
 
 const Header = () => (
   <Content>
     <GradientBackground>
-      <Pagination title="Devices List" />
+      <NavBar title="Devices List" />
     </GradientBackground>
   </Content>
 );
