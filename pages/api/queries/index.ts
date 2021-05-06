41315c5efb76bb916b99a0fea91d7a93b667a790
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2021-05-06 20:21:59 -0300

    Update DEVICES query

--- pages/api/queries/index.ts
+++ pages/api/queries/index.ts
@@ -1,13 +1,18 @@
 import { gql } from '@apollo/client';
 
 export const DEVICES = gql`
-  query Devices {
-    devices {
+  query Devices($page: Int, $search: String) {
+    devices(page: $page, search: $search) {
       data {
         code
         logo
         name
         description: description_fr
+      }
+      pagination {
+        currentPage: current_page
+        hasPreviousPage: has_previous_page
+        hasNextPage: has_next_page
       }
     }
   }
